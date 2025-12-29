70c718f165a5e700b07346e7d8394f5bfa8dac57
Moorthy Chandiran <moorthy.chandiran21@gmail.com>
2025-12-29 18:23:47 +0530

    remove unwanted file

--- src/utils/imageUtils.js
+++
@@ -1,7 +0,0 @@
-export const getImageBaseUrl = (imageUrlArray = [], imageFor) => {
-  return imageUrlArray.find((i) => i.image_for === imageFor)?.image_url || "";
-};
-
-export const getNoImageUrl = (imageUrlArray = []) => {
-  return imageUrlArray.find((i) => i.image_for == "No Image")?.image_url || "";
-};
